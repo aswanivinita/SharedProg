84612a3d173723c80dc6bf0eda9fab71334adabc
aswanivinita <55902733+aswanivinita@users.noreply.github.com>
2020-02-22 22:58:08 -0800

    Replace Sub String Program

---
+++ 37ReplaceSubString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37ReplaceSubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string myString = Console.ReadLine();
+
+            Console.WriteLine("Enter a sub String to check");
+            string checkString = Console.ReadLine();
+
+            bool flag = true;
+
+            for (int i = 0; i < checkString.Length; i++)
+            {
+                for (int j = 0; j < myString.Length; j++)
+                {
+
+                    if (myString[j] == checkString[i])
+                    {
+                        flag = true;
+                        continue;
+                    }
+
+                }
+            }
+
+            if (flag == true)
+                Console.WriteLine(" PResent");
+            else
+                Console.WriteLine("not PResent");
+
+
+        }
+    }
+}
